b5b7ab2c2be6f7c02fb02fd47c60297b57358005
Arb Rahim Badsa <34975329+Arbrahimbadsa@users.noreply.github.com>
2023-06-13 14:37:48 +0000

    tos key added

--- app/tos/page.js
+++ app/tos/page.js
@@ -4,8 +4,8 @@ export default function TOS() {
   return (
     <div className="bg-[#EFF2F7] h-auto w-full p-6">
       <div className="flex flex-col md:w-[80%] md:mx-auto xl:w-[50%] xl:mx-auto">
-        {tosData.sections.map((section) => (
-          <div className="bg-white my-4 p-6 rounded-md">
+        {tosData.sections.map((section, i) => (
+          <div key={i} className="bg-white my-4 p-6 rounded-md">
             <h3 className="font-bold text-xl mb-2">{section.title}</h3>
             <p>{section.description}</p>
           </div>

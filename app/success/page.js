c381b94d4bd8443a015972eeafcef4b055ff9b14
Arb Rahim Badsa <34975329+Arbrahimbadsa@users.noreply.github.com>
2023-06-13 15:50:01 +0000

    imported useRouter from next/navigation

--- app/success/page.js
+++ app/success/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Success() {

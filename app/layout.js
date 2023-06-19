02c89f8032600dc404dfbb8d61192f0ab1fc3124
Arb Rahim Badsa <34975329+Arbrahimbadsa@users.noreply.github.com>
2023-06-19 16:09:13 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hire Me - The most easiest way to hire me now.",
+  title: "Hire Me - The easiest way to hire me now.",
   description:
     "A simple website to get involved in a project with me. Type your email, set your project and budget. All set!",
 };
